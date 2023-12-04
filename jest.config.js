09d819563a0c2c193b060fd984f084d50ce71768
Freddy Fabian <99642629+Freddy444@users.noreply.github.com>
2023-12-04 13:33:03 -0500

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['/node_modules/'],
+};
