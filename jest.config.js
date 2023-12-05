c09cbc0ad01d7b71d774eecd130508bd646f7302
Freddy Fabian <99642629+Freddy444@users.noreply.github.com>
2023-12-04 20:45:06 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
-{
-  "moduleNameMapper": {
+/*This file deals with css and static assets that cannot be loaded through jest, as well as modules which aren't already read by jest..*/
+
+module.exports = {
+  moduleNameMapper: {
     "\\.(scss|sass|css)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
   },
-  "transformIgnorePatterns": ["node_modules/(?!(axios)/)"],
-  "transform": {
+  //This snippet adapted from: https://dev.to/steveruizok/jest-and-esm-cannot-use-import-statement-outside-a-module-4mmj
+  transformIgnorePatterns: ["node_modules/(?!(axios)/)"],
+  transform: {
     "^.+\\.(js|jsx|ts|tsx|mjs)$": "babel-jest",
   },
-  "type": "module",  // Add this line to support ECMAScript Modules
-  "extensionsToTreatAsEsm": [".js", ".jsx"]  // Add this line to specify file extensions for ESM
-}
-
+};
