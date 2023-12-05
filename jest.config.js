ea99e412e3dd0200f119efcc64923196038470dc
Freddy Fabian <99642629+Freddy444@users.noreply.github.com>
2023-12-04 20:42:27 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
-
-module.exports = {
-  moduleNameMapper: {
+{
+  "moduleNameMapper": {
     "\\.(scss|sass|css)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
   },
-  //This snippet adapted from: https://dev.to/steveruizok/jest-and-esm-cannot-use-import-statement-outside-a-module-4mmj
-  transformIgnorePatterns: ["node_modules/(?!(axios)/)"],
-  transform: {
+  "transformIgnorePatterns": ["node_modules/(?!(axios)/)"],
+  "transform": {
     "^.+\\.(js|jsx|ts|tsx|mjs)$": "babel-jest",
   },
-};
+  "type": "module",  // Add this line to support ECMAScript Modules
+  "extensionsToTreatAsEsm": [".js", ".jsx"]  // Add this line to specify file extensions for ESM
+}
+
