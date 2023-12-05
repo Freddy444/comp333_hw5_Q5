854590bdc659b27a9ac52038ae6629813f902f01
Freddy Fabian <99642629+Freddy444@users.noreply.github.com>
2023-12-04 21:07:01 -0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
-/*This allows jest to work with react.*/
-module.exports = {presets: ['@babel/preset-env',['@babel/preset-react',{
-        //This deals with "React is not defined" error. Pulled from React17 Documentation.
-        "runtime": "automatic"
-        }]
-    ]
-}
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', {
+      // This deals with "React is not defined" error. Pulled from React17 Documentation.
+      "runtime": "automatic"
+    }],
+    '@babel/preset-modules', // Add this line for ESM support
+    '@babel/preset-typescript', // Add this line if using TypeScript
+  ]
+};
